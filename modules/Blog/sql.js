6c80fe8351b323ca125973ccf7b3844855e19736
ever <892938772@qq.com>
2020-06-02 11:07:02 +0800

    添加sql

--- modules/Blog/sql.js
+++ modules/Blog/sql.js
@@ -1,15 +1,7 @@
 //sql.js
 // SQL语句封裝
-var sql = {
-    insert: 'INSERT INTO y_blogs(blog_id, blog_title, blog_date, blog_updated_date, blog_tags, blog_description, blog_is_recommended, blog_route) VALUES(?,?,?,?,?,?,?,?)',
-    batchInsert: 'INSERT INTO y_blogs(blog_id, blog_title, blog_date, blog_updated_date, blog_tags, blog_description, blog_is_recommended, blog_route) VALUES ?',
+let sql = {
     updateDate: 'UPDATE y_blogs SET blog_updated_date=? WHERE blog_id=?',
-    delete: 'DELETE FROM y_blogs WHERE blog_id=?',
-    deleteAll: 'DELETE FROM y_blogs',
-    queryById: 'SELECT * FROM y_blogs WHERE blog_id=?',
-    queryAll: 'SELECT * FROM y_blogs',
-    query: 'SELECT * FROM y_blogs',
-    queryAllBlogs: 'SELECT  blog_id `key` ,blog_title title,  DATE_FORMAT(blog_date,\'%Y-%m-%d\') `date`, blog_tags tag, blog_description summary, blog_is_recommended recommended, blog_route route FROM y_blogs;',
 };
 
 module.exports = sql;
--- modules/User/sql.js
+++ modules/User/sql.js
@@ -1,6 +1,6 @@
 //sql.js
 // SQL语句封裝
-var user = {
+let user = {
     updatePassword: 'UPDATE y_users SET password=? WHERE id=?',
     update: 'update y_users set `name`=?, gender=?, password=?, role=?, mail_address=?, phone_number=?, age=?, creation_date=? WHERE id=?',
 };
