5aa5a023474f4a69973f6fe3dce815522c86d338
ever <892938772@qq.com>
2020-06-11 17:58:21 +0800

    https

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header("Content-Security-Policy: upgrade-insecure-requests");
     next();
 });
 
