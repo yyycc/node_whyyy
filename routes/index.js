924bf0f33ad2e0ec2ec54fa0596a6fe5e932092a
ever <892938772@qq.com>
2020-06-17 16:10:57 +0800

    配置http和https监听 优化/

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.json({
+    users: [
+        "/",
+        "/queryById",
+        "/query",
+        "/queryAll",
+        "/deleteById",
+        "/deleteByIds",
+    ],
+    blog: [
+      "/",
+      "/queryById",
+      "/query",
+      "/queryAll",
+      "/deleteById",
+      "/deleteByIds",
+    ]
+  });
 });
 
 module.exports = router;
